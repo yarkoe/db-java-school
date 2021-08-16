0223765786bf76a3817128f0508a35d1c22ddbfc
Koekin Yaroslav <ykoekin@gmail.com>
2021-08-16 17:29:58 +0300

    Add SuperIntegerList Interface

---
+++ src/main/java/homework/lab1/SuperIntegerList.java
@@ -0,0 +1,13 @@
+package homework.lab1;
+
+public interface SuperIntegerList {
+    void add(int number);
+
+    void removeByIndex(int index);
+
+    void removeByValue(int value);
+
+    int get(int index);
+
+    void printAll();
+}
