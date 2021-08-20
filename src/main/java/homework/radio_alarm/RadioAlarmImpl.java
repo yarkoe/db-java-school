a4f5c178ffa5679a0990a1017b89a7812d688c96
Koekin Yaroslav <ykoekin@gmail.com>
2021-08-20 15:52:32 +0300

    Add radio and alarm composition to RadioAlarm

--- src/main/java/homework/radio_alarm/RadioAlarmImpl.java
+++ src/main/java/homework/radio_alarm/RadioAlarmImpl.java
@@ -1,23 +1,31 @@
 package homework.radio_alarm;
 
 public class RadioAlarmImpl implements RadioAlarm {
+    private final Radio radio;
+    private final Alarm alarm;
+
+    public RadioAlarmImpl(Radio radio, Alarm alarm) {
+        this.radio = radio;
+        this.alarm = alarm;
+    }
+
     @Override
     public void c() {
-
+        alarm.c();
     }
 
     @Override
     public void d() {
-
+        alarm.d();
     }
 
     @Override
     public void a() {
-
+        radio.a();
     }
 
     @Override
     public void b() {
-
+        radio.b();
     }
 }
