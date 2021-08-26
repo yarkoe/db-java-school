ba01bf7c0e10cbdebc8690dcb1d938055f2b2daf
Koekin Yaroslav <ykoekin@gmail.com>
2021-08-26 15:05:42 +0300

    Give console speaker creation to object factory

--- src/main/java/my_spring/robot/SpeakerWithTimeDecorator.java
+++ src/main/java/my_spring/robot/SpeakerWithTimeDecorator.java
@@ -1,10 +1,11 @@
 package my_spring.robot;
 
 import lombok.Setter;
+import my_spring.object_factory.ObjectFactory;
 
 public class SpeakerWithTimeDecorator implements Speaker {
     @Setter
-    private Speaker speaker = new ConsoleSpeaker();
+    private Speaker speaker = ObjectFactory.getInstance().createObject(ConsoleSpeaker.class);
 
     @Override
     public void speak(String str) {
