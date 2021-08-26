9d513c7f0ce847ede7864399886f9da558b102b2
Koekin Yaroslav <ykoekin@gmail.com>
2021-08-26 16:27:10 +0300

    Fix ConsoleSpeakerWithDecoratorImpl

--- src/main/java/my_spring/robot/ConsoleSpeakerWithDecoratorImpl.java
+++ src/main/java/my_spring/robot/ConsoleSpeakerWithDecoratorImpl.java
@@ -1,10 +1,15 @@
 package my_spring.robot;
 
+import my_spring.object_factory.annotation.InjectByType;
+
 public class ConsoleSpeakerWithDecoratorImpl extends ConsoleSpeaker {
+    @InjectByType
+    private ConsoleSpeaker consoleSpeaker;
+
     @Override
     public void speak(String str) {
         long startTime = System.nanoTime();
-        super.speak(str);
+        consoleSpeaker.speak(str);
         long endTime = System.nanoTime();
         System.out.println(endTime - startTime);
     }
